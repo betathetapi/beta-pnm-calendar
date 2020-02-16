92fe3faf90a0232cc64f7d8c3eb9f7d8db7cee40
AJ Jordan <alex@strugee.net>
2020-02-16 00:06:07 -0500

    Get an MVP working

---
+++ index.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var whenDomReady = require('when-dom-ready');
+
+// XXX SECURITY make sure we're not including any secrets here
+var config = require('./config');
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+	gapi.load('client:auth2', initClient);
+}
+// Need this because Browserify wraps in an IIFE
+window.handleClientLoad = handleClientLoad;
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+	gapi.client.init({
+		apiKey: config.api_key,
+		clientId: config.client_id,
+		discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+		scope: "https://www.googleapis.com/auth/calendar"
+	}).then(function () {
+		// Listen for sign-in state changes.
+		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+		// Handle the initial sign-in state.
+		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+		authorizeButton.onclick = handleAuthClick;
+		signoutButton.onclick = handleSignoutClick;
+	}, function(error) {
+		appendPre(JSON.stringify(error, null, 2));
+	});
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+	if (isSignedIn) {
+		authorizeButton.style.display = 'none';
+		signoutButton.style.display = 'block';
+		listUpcomingEvents();
+	} else {
+		authorizeButton.style.display = 'block';
+		signoutButton.style.display = 'none';
+	}
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+	gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+	gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+	var pre = document.getElementById('content');
+	var textContent = document.createTextNode(message + '\n');
+	pre.appendChild(textContent);
+}
+
+function listUpcomingEvents() {
+	gapi.client.calendar.calendarList.insert({
+		id: config.calendar_id
+	}).then(function(response) {
+		console.dir(response.result);
+	});
+
+	gapi.client.calendar.events.list({
+		'calendarId': 'primary',
+		'timeMin': (new Date()).toISOString(),
+		'showDeleted': false,
+		'singleEvents': true,
+		'maxResults': 10,
+		'orderBy': 'startTime'
+	}).then(function(response) {
+		var events = response.result.items;
+		appendPre('Upcoming events:');
+
+		if (events.length > 0) {
+			for (i = 0; i < events.length; i++) {
+				var event = events[i];
+				var when = event.start.dateTime;
+				if (!when) {
+					when = event.start.date;
+				}
+				appendPre(event.summary + ' (' + when + ')')
+			}
+		} else {
+			appendPre('No upcoming events found.');
+		}
+	});
+}
+
